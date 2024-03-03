240eff45cf7567dab695cdf62e58395a676ec654
Animesh Paul <ani.nitmz@gmail.com>
2024-03-02 21:19:01 -0800

    Test

--- spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ spring-boot-microservices-workshop/movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
