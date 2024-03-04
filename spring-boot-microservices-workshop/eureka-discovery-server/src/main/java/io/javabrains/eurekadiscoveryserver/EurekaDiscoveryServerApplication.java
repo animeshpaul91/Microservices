4547dd0ebba4328fe06fbc5643c44be6b66e1b66
Animesh Paul <ani.nitmz@gmail.com>
2024-03-03 20:48:33 -0800

    Added my own discovery-server

--- spring-boot-microservices-workshop/eureka-discovery-server/src/main/java/io/javabrains/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
+++ spring-boot-microservices-workshop/eureka-discovery-server/src/main/java/io/javabrains/eurekadiscoveryserver/EurekaDiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package io.javabrains.discoveryserver;
+package io.javabrains.eurekadiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,11 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServerApplication {
+public class EurekaDiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
+		SpringApplication.run(EurekaDiscoveryServerApplication.class, args);
 	}
 
 }
-
