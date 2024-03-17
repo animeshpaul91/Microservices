511f9151eee14d8a9def1c77d93e0a310746f897
Animesh Paul <ani.nitmz@gmail.com>
2024-03-16 19:15:56 -0700

    Added ways to add @Value Annotation in Spring Properties File

--- spring-boot-microservices-workshop/spring-boot-config/src/main/java/io/javabrains/springbootconfig/controllers/GreetingController.java
+++ spring-boot-microservices-workshop/spring-boot-config/src/main/java/io/javabrains/springbootconfig/controllers/GreetingController.java
@@ -4,13 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default}") // :default is the default value if the property does not exist
     private String greetingMessage;
+
+    @Value("Static Message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
+
+    @Value("#{${dbValues}}") // #{} converts it into a spring expression language
+    private Map<String, String> dbValues;
+
     @GetMapping("/greeting")
     public String greeting() {
-        return greetingMessage;
+        return greetingMessage + " " + staticMessage + " " + listValues + " " + dbValues;
     }
 }
