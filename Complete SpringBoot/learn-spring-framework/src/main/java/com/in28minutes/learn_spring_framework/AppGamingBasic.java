87b48112ceae040b8cfef443fd7e670f62ec0daf
Animesh Paul <ani.nitmz@gmail.com>
2025-06-01 21:30:15 -0700

    Added Pacman Game

--- Complete SpringBoot/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
+++ Complete SpringBoot/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasic.java
@@ -1,15 +1,16 @@
 package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
 import com.in28minutes.learn_spring_framework.game.MarioGame;
 import com.in28minutes.learn_spring_framework.game.PacmanGame;
 import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
 public class AppGamingBasic {
     public static void main(String[] args) {
-        final MarioGame marioGame = new MarioGame();
-        final SuperContraGame superContraGame = new SuperContraGame();
-        final PacmanGame pacmanGame = new PacmanGame();
+        final GamingConsole marioGame = new MarioGame();
+        final GamingConsole superContraGame = new SuperContraGame();
+        final GamingConsole pacmanGame = new PacmanGame();
         final GameRunner gameRunner = new GameRunner(pacmanGame);
         gameRunner.run();
     }
