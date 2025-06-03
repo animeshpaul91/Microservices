43ec985b56a70266b229de8fd75b7b923c063e3a
Animesh Paul <ani.nitmz@gmail.com>
2025-06-02 21:48:34 -0700

    Added Primary and Qualifier

--- Complete SpringBoot/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldSpringApp.java
+++ Complete SpringBoot/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldSpringApp.java
@@ -4,6 +4,8 @@ import com.in28minutes.learn_spring_framework.configuration.HelloWorldConfigurat
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class HelloWorldSpringApp {
     public static void main(String[] args) {
         // 1. Launch a Spring Context with our Configuration
@@ -18,5 +20,8 @@ public class HelloWorldSpringApp {
         System.out.println(context.getBean("getPerson"));
         System.out.println(context.getBean("getAddress"));
         System.out.println(context.getBean("getPersonWithParameters"));
+        System.out.println(context.getBean("getPersonWithQualifier"));
+        System.out.println("-------------------");
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
     }
 }
--- Complete SpringBoot/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/configuration/HelloWorldConfiguration.java
+++ Complete SpringBoot/learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/configuration/HelloWorldConfiguration.java
@@ -2,8 +2,10 @@ package com.in28minutes.learn_spring_framework.configuration;
 
 import com.in28minutes.learn_spring_framework.dto.Address;
 import com.in28minutes.learn_spring_framework.dto.Person;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 // Configure beans that we want Spring to manage
 @Configuration
@@ -32,7 +34,18 @@ public class HelloWorldConfiguration {
     }
 
     @Bean
+    public Person getAnotherPerson(String getName, int getAge, Address address) { // getName(), getAge(), getAddress()
+        return new Person(getName, getAge, address);
+    }
+
+    @Bean
+    public Person getPersonWithQualifier(String getName, int getAge, @Qualifier("CO") Address address) { // getName(), getAge(), getAddress()
+        return new Person(getName, getAge, address);
+    }
+
+    @Bean
     //(name = "address")
+    @Primary // resolves conflicts for multiple beans of same type
     public Address getAddress() {
         return new Address("89 Spring Ville Ave", "Buffalo", "NY", 14226);
     }
@@ -40,5 +53,11 @@ public class HelloWorldConfiguration {
     @Bean
     public Address getAddress2() {
         return new Address("7001 Old Redmond Road", "Redmond", "WA", 98052);
+    }
+
+    @Bean
+    @Qualifier("CO")
+    public Address getAddress3() {
+        return new Address("492 W Burgundy St", "Highlands Ranch", "CO", 80129);
     }
 }
