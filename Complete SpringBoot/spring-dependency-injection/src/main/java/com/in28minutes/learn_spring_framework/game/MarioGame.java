112bb49b1b06490ce8093c5ba7044b2c0da2f0d0
Animesh Paul <ani.nitmz@gmail.com>
2025-07-12 18:43:05 -0700

    Added Qualifier to resolve Conflicts in dependency Injection

--- Complete SpringBoot/spring-dependency-injection/src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
+++ Complete SpringBoot/spring-dependency-injection/src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
@@ -1,9 +1,11 @@
 package com.in28minutes.learn_spring_framework.game;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 @Qualifier("MarioGame")
 public class MarioGame implements GamingConsole {
     @Override
