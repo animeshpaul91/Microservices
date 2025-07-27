bc170e086fbfbac780474a129a1d3583dba4a9ed
Animesh Paul <ani.nitmz@gmail.com>
2025-07-26 21:47:29 -0700

    Checkpoint

--- Websockets/Chat_Application/client.js
+++ Websockets/Chat_Application/client.js
@@ -1,27 +1,39 @@
 const { io } = require('socket.io-client');
-const socket = io('http://localhost:3000'); // Connect to the server
 
+// Connect to the Socket.IO server running on localhost:3000
+const socket = io('http://localhost:3000');
+
+
+// When the client successfully connects to the server
 socket.on('connect', () => {
     console.log(`You are now connected to server with ID ${socket.id}`);
     console.log('Sending message to server every second for 10 seconds');
 
+    // Send a message to the server every second for 10 seconds
     const intervalId = setInterval(() => {
-        socket.emit('send-message', 'Hello from client!'); // Emit a custom event with a message
-    }, 1000); // send every 1 second
+        // Emit a custom event 'send-message' with a message payload
+        socket.emit('send-message', 'Hello from client!');
+    }, 1000);
 
+    // Stop sending messages after 10 seconds
     setTimeout(() => {
-        clearInterval(intervalId); // stop after 10 seconds
+        clearInterval(intervalId);
     }, 10000);
 
-    socket.on('receive-message', (data) => { // Listen for messages from the server
+    // Listen for 'receive-message' events from the server
+    socket.on('receive-message', (data) => {
         console.log('Received from server: ' + data);
     });
 });
 
+
+// When the client disconnects from the server
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
 });
 
+
+// Handle connection errors
 socket.on('error', (error) => {
     console.error('Socket.IO error: ' + error);
 });
