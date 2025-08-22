11b7f2b9618bd18cdb96211eb73560e900a437bf
Animesh Paul <ani.nitmz@gmail.com>
2025-08-21 18:53:54 -0700

    Test commit

--- Complete SpringBoot/spring-dependency-injection/src/main/java/com/in28minutes/learn_spring_framework/configuration/GamingConfiguration.java
+++ Complete SpringBoot/spring-dependency-injection/src/main/java/com/in28minutes/learn_spring_framework/configuration/GamingConfiguration.java
@@ -6,7 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.in28minutes.learn_spring_framework.game")
 public class GamingConfiguration {
-    // This class is used to configure the Spring application context for gaming-related beans.
-    // It uses @Configuration to indicate that it provides Spring configuration,
-    // and @ComponentScan to specify the package to scan for components.
 }
