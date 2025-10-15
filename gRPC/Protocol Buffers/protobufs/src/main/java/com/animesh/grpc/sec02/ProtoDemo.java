2cd3d46cf73e83eeeee110b462b736718ebbd427
Animesh Paul <ani.nitmz@gmail.com>
2025-10-14 20:39:39 -0700

    Added multiple files with same name

--- gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec02/ProtoDemo.java
+++ gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec02/ProtoDemo.java
@@ -1,12 +1,11 @@
-package com.animesh.grpc.sec01;
+package com.animesh.grpc.sec02;
 
-
-import com.animesh.grpc.sec01.models.Person;
+import com.animesh.grpc.sec02.models.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class SimpleProtoDemo {
-    private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+public class ProtoDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProtoDemo.class);
 
     public static void main(String[] args) {
         final Person person = Person.newBuilder()
