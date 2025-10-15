d537d753f5fbe196f7226271d16bc3ed0c3c11f3
Animesh Paul <ani.nitmz@gmail.com>
2025-10-14 20:16:30 -0700

    Added simple person proto class

---
+++ gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec01/SimpleProtoDemo.java
@@ -0,0 +1,20 @@
+package com.animesh.grpc.sec01;
+
+
+import com.animesh.grpc.models.PersonOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemo {
+    private static final Logger log = LoggerFactory.getLogger(SimpleProtoDemo.class);
+
+    public static void main(String[] args) {
+        final PersonOuterClass.Person person = PersonOuterClass.Person.newBuilder()
+                .setName("John")
+                .setAge(12)
+                .build();
+
+        log.info("Person Name: {}", person.getName());
+        log.info("Person Age: {}", person.getAge());
+    }
+}
