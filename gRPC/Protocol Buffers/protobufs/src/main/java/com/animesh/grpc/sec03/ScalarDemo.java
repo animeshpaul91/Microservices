bf6ebfa9a289e9e229859cf50e40cd045e0645a2
Animesh Paul <ani.nitmz@gmail.com>
2025-10-15 20:23:24 -0700

    Revisited Scalars

---
+++ gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec03/ScalarDemo.java
@@ -0,0 +1,23 @@
+package com.animesh.grpc.sec03;
+
+import com.animesh.grpc.sec03.models.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScalarDemo {
+    private static final Logger log = LoggerFactory.getLogger(ScalarDemo.class);
+
+    public static void main(String[] args) {
+        final Person person = Person.newBuilder()
+                .setLastName("Paul")
+                .setAge(32)
+                .setEmail("ani.nitmz@gmail.com")
+                .setEmployed(true)
+                .setSalary(5000)
+                .setBankAccountNumber(200054231)
+                .setBalance(-1000)
+                .build();
+
+        log.info("Person: {}", person);
+    }
+}
