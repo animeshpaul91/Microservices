0c376382fe8387211bc3c2a3d95f032d40dbea84
Animesh Paul <ani.nitmz@gmail.com>
2025-10-21 21:30:09 -0700

    Added imports

---
+++ gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec04/ImportDemo.java
@@ -0,0 +1,40 @@
+package com.animesh.grpc.sec04;
+
+import com.animesh.grpc.sec03.models.Credentials;
+import com.animesh.grpc.sec03.models.Email;
+import com.animesh.grpc.sec03.models.Phone;
+import com.animesh.grpc.sec04.models.Person;
+import com.animesh.grpc.sec04.models.common.Address;
+import com.animesh.grpc.sec04.models.common.BodyStyle;
+import com.animesh.grpc.sec04.models.common.Car;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ImportDemo {
+    private static final Logger log = LoggerFactory.getLogger(ImportDemo.class);
+
+    public static void main(String[] args) {
+        final Address address = Address.newBuilder()
+                .setStreet("123 Main St")
+                .setCity("Springfield")
+                .setState("IL")
+                .setZipCode("62701")
+                .build();
+
+        final Car car = Car.newBuilder()
+                .setMake("Toyota")
+                .setModel("Camry")
+                .setYear(2020)
+                .setBodyStyle(BodyStyle.SEDAN)
+                .build();
+
+        final Person person = Person.newBuilder()
+                .setName("John Doe")
+                .setAge(30)
+                .setAddress(address)
+                .setCar(car)
+                .build();
+
+        log.info("Person Details: {}", person);
+    }
+}
