e7c48ad8ea533c2099b7b54c36fdac09a11976b1
Animesh Paul <ani.nitmz@gmail.com>
2025-10-21 21:44:09 -0700

    Added well known types

---
+++ gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec04/WellKnownTypesDemo.java
@@ -0,0 +1,26 @@
+package com.animesh.grpc.sec04;
+
+import com.animesh.grpc.sec04.models.Sample;
+import com.google.protobuf.Int32Value;
+import com.google.protobuf.Timestamp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+
+public class WellKnownTypesDemo {
+    private static final Logger log = LoggerFactory.getLogger(WellKnownTypesDemo.class);
+
+    public static void main(String[] args) {
+        final Sample sample = Sample.newBuilder()
+                .setAge(Int32Value.of(123))
+                .setLoginTime(Timestamp.newBuilder()
+                        .setSeconds(Instant.now().getEpochSecond())
+                        .build())
+                .build();
+
+        log.info("sample has age: {}", sample.hasAge());
+        log.info("sample age: {}", sample.getAge().getValue());
+        log.info("{}", Instant.ofEpochSecond(sample.getLoginTime().getSeconds()));
+    }
+}
