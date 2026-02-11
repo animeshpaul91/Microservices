bb0fab559f8ca314f41fdbdf04dfd0c77b717c85
Animesh Paul <ani.nitmz@gmail.com>
2026-02-10 22:02:42 -0800

    Added first gRPC Service

---
+++ gRPC/Protocol Buffers/protobufs/src/main/java/com/animesh/grpc/sec06/BankService.java
@@ -0,0 +1,21 @@
+package com.animesh.grpc.sec06;
+
+import com.animesh.grpc.sec06.models.AccountBalance;
+import com.animesh.grpc.sec06.models.BalanceCheckRequest;
+import com.animesh.grpc.sec06.models.BankServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+// Server side implementation of the BankService (Service Class)
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+    @Override
+    public void getAccountBalance(BalanceCheckRequest request, StreamObserver<AccountBalance> responseObserver) {
+        final var accountNumber = request.getAccountNumber();
+        final var accountBalance = AccountBalance.newBuilder()
+                .setAccountNumber(accountNumber)
+                .setBalance(1000)
+                .build();
+
+        responseObserver.onNext(accountBalance);
+        responseObserver.onCompleted();
+    }
+}
