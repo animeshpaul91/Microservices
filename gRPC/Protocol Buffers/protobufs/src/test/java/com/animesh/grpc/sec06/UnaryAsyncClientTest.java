288c5445a0550a2e1bc4cbf344c5727a8f80a7a2
Animesh Paul <ani.nitmz@gmail.com>
2026-04-13 21:17:55 -0700

    Refactored Test

--- gRPC/Protocol Buffers/protobufs/src/test/java/com/animesh/grpc/sec06/UnaryAsyncClientTest.java
+++ gRPC/Protocol Buffers/protobufs/src/test/java/com/animesh/grpc/sec06/UnaryAsyncClientTest.java
@@ -1,22 +1,31 @@
 package com.animesh.grpc.sec06;
 
 import com.animesh.grpc.common.ResponseObserver;
+import com.animesh.grpc.sec06.models.AccountBalance;
 import com.animesh.grpc.sec06.models.BalanceCheckRequest;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.CountDownLatch;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class UnaryAsyncClientTest extends AbstractTest {
     private static final Logger log = LoggerFactory.getLogger(UnaryAsyncClientTest.class);
 
     @Test
-    void testGetBalance() throws InterruptedException {
+    void testGetBalance() {
         var request = BalanceCheckRequest.newBuilder()
                 .setAccountNumber(2000542313)
                 .build();
 
-        this.asyncStub.getAccountBalance(request, ResponseObserver.create());
+        final ResponseObserver<AccountBalance> accountBalanceObserver = ResponseObserver.create();
+        this.asyncStub.getAccountBalance(request, accountBalanceObserver);
+        accountBalanceObserver.await(); // main thread waits in latch.await() until latch.countdown() is called either in happy or sad paths
+
+        // at this point either list or error will be populated
+        assertEquals(1, accountBalanceObserver.getList().size());
+        assertEquals(100, accountBalanceObserver.getList().getFirst().getBalance());
+        assertNull(accountBalanceObserver.getError());
     }
 }
